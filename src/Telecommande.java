396b7cb55f72e0d4e7de4d2cd783c753267fa3a8
El Bakhtaoui Wassim <wassim.el-bakhtaoui9@etu.univ-lorraine.fr>
2021-04-13 10:04:39 +0200

    Ajout de la classe Telecommande.java remplie

---
+++ src/Telecommande.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+
+/**
+ * Classe Telecommande chargée de commander plusieurs lampes à distance
+ */
+public class Telecommande {
+
+    /**
+     * liste des lampes controlées par la télécommande
+     */
+    private List<Lampe> lampes;
+
+
+
+    /**
+     * constructeur vide qui construit une télécommande ne controlant initialement aucune lampe
+     */
+    public Telecommande(){
+
+        throw new error("Pas rempli");
+
+    }
+    
+    /**
+     * ajoute un lien dans la télécommande permettant de commander cette lampe
+     * @param Lampe une lampe
+     */
+    public void ajouterLampe(Lampe pLampe){
+
+        throw new error("Pas rempli");
+
+    }
+    
+    /**
+     *  
+     */
+    public void activerLampe(int indiceLampe){
+
+        throw new error("Pas rempli");
+
+    }
+
+
+    public void desactiverLampe(int indiceLampe){
+
+        throw new error("Pas rempli");
+
+    }
+
+    public void activerTout(){
+
+        throw new error("Pas rempli");
+
+    }
+
+
+
+
+
+
+}
